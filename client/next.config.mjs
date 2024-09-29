d3e21d385179fdedb8ed6b94dc812d8bf737ab49
Amaan Hussain <Hussainamaan87@gmail.com>
2024-09-29 21:35:13 +0530

    updates global css

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images : {
-        remotePatterns : [
-            {
-                protocol : "https",
-                hostname : "pm-s3-images-x.s3.ap-south-1.amazonaws.com",
-                port : "",
-                pathname : "/**",
-            }
-        ]
-    }
-};
-
-export default nextConfig;
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'pm-s3-images-x.s3.ap-south-1.amazonaws.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
